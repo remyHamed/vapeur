e87e032a053ca87bf5103c92856fdd5c6b67d344
matthieudupas <matthieudupas@outlook.fr>
2023-02-02 12:14:57 +0100

    home fragment

--- app/src/main/java/com/example/vapeur/description.kt
+++ app/src/main/java/com/example/vapeur/description.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [forgotten_password.newInstance] factory method to
+ * Use the [description.newInstance] factory method to
  * create an instance of this fragment.
  */
-class forgotten_password : Fragment() {
+class description : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class forgotten_password : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgotten_password, container, false)
+        return inflater.inflate(R.layout.fragment_description, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class forgotten_password : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment forgotten_password.
+         * @return A new instance of fragment description.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            forgotten_password().apply {
+            description().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
